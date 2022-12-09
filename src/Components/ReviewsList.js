d9b21318e6a50c8d516367f7f1455492e7b3995b
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2022-12-08 20:58:12 -0500

    created ReviewsListJs

--- src/Components/ReviewsList.js
+++ src/Components/ReviewsList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReviewSummary from './ReviewSummary';
 
+
 function ReviewsList({ reviews }) {
 
   const mappedReviews = reviews.map(review => <ReviewSummary key={review.id} review={review} />)
